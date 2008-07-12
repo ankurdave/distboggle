42c286a14b554668d1b97a30ceaf09ef52f52c39
ankur <none@none>
2008-07-12 05:35:31 +0000

    changed ip
    --HG--
    extra : convert_revision : svn%3Af56d47ca-78d0-4a4c-933a-5983747f1da5%40353

--- BoggleClientTester.java
+++ BoggleClientTester.java
@@ -1,5 +1,5 @@
 public class BoggleClientTester {
 	public static void main(String[] args) {
-		new BoggleClient("localhost", 4444, "words.txt", 4).connect();
+		new BoggleClient("192.168.1.223", 4444, "words.txt", 4).connect();
 	}
 }
