c9ac403ad9467ec11968f19f53894170e997bcb6
ankur <none@none>
2008-11-11 20:06:22 +0000

    added failure rate calculator
    --HG--
    extra : convert_revision : svn%3Af56d47ca-78d0-4a4c-933a-5983747f1da5%40393

--- stats/calculate-stats.pl
+++ stats/calculate-stats.pl
@@ -1,12 +1,27 @@
+#!/usr/bin/perl
+
+use warnings;
+
+$mxt = $mxs = 0;
+$mnt = $mns = 100000000;
 while (<>) {
 	next unless /^[A-Z]{16} (\d+) (\d+)$/;
 	($s, $t) = ($1, $2);
 	$tt += $t;
 	$ts += $s;
-	$i++;
+	$mxt = $t if $t > $mxt;
+	$mxs = $s if $s > $mxs;
+	$mnt = $t if $t < $mnt;
+	$mns = $s if $s < $mns;
+	$fail++ if $s < 3500;
+	last if ++$i >= 100;
 }
 
 $tt /= $i;
 $ts /= $i;
+$et = ($mxt - $mnt) / $i;
+$es = ($mxs - $mns) / $i;
+$fail /= $i;
+$fail *= 100;
 
-print "$i $tt $ts\n";
+print "# of datapoints: $i\naverage time: $tt +- $et\naverage score: $ts +- $es\nfailure %: $fail\n";
