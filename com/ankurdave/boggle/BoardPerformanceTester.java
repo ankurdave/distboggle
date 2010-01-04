7dc2d2961d69e21c1de8a8528a1a8c0342a92b78
ankur <none@none>
2010-01-04 23:21:31 +0000

    Added performance tester, best boggle stats
    --HG--
    extra : convert_revision : svn%3Af56d47ca-78d0-4a4c-933a-5983747f1da5%40646

---
+++ com/ankurdave/boggle/BoardPerformanceTester.java
@@ -0,0 +1,30 @@
+package com.ankurdave.boggle;
+
+public class BoardPerformanceTester {
+	private static final int SIDE_LENGTH = 4;
+	private static final int NUM_BOARDS = 1000;
+	
+	public static void main(String[] args) {
+		char[][] grid = new char[SIDE_LENGTH][SIDE_LENGTH];
+		Board board;
+		Dictionary dict = new Dictionary();
+		dict.buildDictionary("/home/ankur/notes/yawl.txt");
+		
+		long startTime = System.nanoTime();
+		for (int iter = 0; iter < NUM_BOARDS; iter++) {
+			for (int i = 0; i < SIDE_LENGTH; i++) {
+				for (int j = 0; j < SIDE_LENGTH; j++) {
+					grid[i][j] = (char) (Math.random() * (90 - 65 + 1) + 65);
+				}
+			}
+			board = new Board(grid, dict);
+		
+			board.generate();
+			
+			System.out.println(board);
+		}
+		long time = System.nanoTime() - startTime;
+		double secPerBoard = (double)time / NUM_BOARDS * 1e-9;
+		System.out.println(NUM_BOARDS + " boards in " + time + " ns. " + secPerBoard + " sec/board.");
+	}
+}
