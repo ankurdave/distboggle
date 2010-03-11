45355ef68421460b1f9f05cdd0820eb477f5af7b
Ankur Dave <ankurdave@gmail.com>
2010-03-10 20:19:29 -0800

    Removed old testing code for the Extended Essay -- it was resetting clients periodically for benchmarking

--- com/ankurdave/boggle/Server.java
+++ com/ankurdave/boggle/Server.java
@@ -1,15 +1,17 @@
 package com.ankurdave.boggle;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collections;
+
 /**
  * Server component of DistBoggle. Manages BoggleClients.
+ * 
  * @author ankur
  */
 public class Server {
-	private int trials = 1;
 	private static final int DEFAULT_POP_CAP = 20;
 	private static final int POP_CAP_RANGE = 0;
 	private int curClientID = 0;
@@ -18,13 +20,13 @@ public class Server {
 	private ServerSocket socket;
 	private long startTime;
 	private ArrayList<ServerThread> threads = new ArrayList<ServerThread>();
+	
 	public Server(int port) {
 		// create the socket
 		socket = null;
 		try {
 			socket = new ServerSocket(port);
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.err.println("Could not listen on port " + port);
 			System.exit(1);
 		}
@@ -32,41 +34,49 @@ public class Server {
 		dict = new Dictionary();
 		dict.buildDictionary("words.txt");
 	}
+	
 	// TODO analyze migrant allocation algorithm
 	public synchronized void addMigrant(Board migrant, ServerThread caller) {
 		Collections.sort(threads);
 		for (ServerThread c : threads) {
 			if ((c.getMigrant() == null || c.getMigrant().getScore() < migrant
-			        .getScore())
-			        && caller != c) {
+					.getScore())
+					&& caller != c) {
 				c.setMigrant(migrant);
 				break;
 			}
 		}
 	}
+	
 	public synchronized Dictionary getDictionary() {
 		return dict;
 	}
+	
 	// TODO analyze variable pop cap
 	public synchronized int getPopCapForClient(int clientID) {
 		Collections.sort(threads);
-		if (threads.size() == 1) { return DEFAULT_POP_CAP; }
+		if (threads.size() == 1) {
+			return DEFAULT_POP_CAP;
+		}
 		for (int i = 0; i < threads.size(); i++) {
-			if (threads.get(i).getId() == clientID) { return (DEFAULT_POP_CAP + POP_CAP_RANGE / 2)
-			        - i * (POP_CAP_RANGE / (threads.size() - 1)); }
+			if (threads.get(i).getId() == clientID) {
+				return (DEFAULT_POP_CAP + POP_CAP_RANGE / 2)
+						- i * (POP_CAP_RANGE / (threads.size() - 1));
+			}
 		}
 		return DEFAULT_POP_CAP;
 	}
+	
 	/**
-     * Starts listening for clients. Starts a new thread for each client. Never
-     * returns.
-     */
+	 * Starts listening for clients. Starts a new thread for each client. Never
+	 * returns.
+	 */
 	public void listen() {
 		try {
 			while (true) {
 				Socket s = socket.accept();
 				ServerThread serverThread = new ServerThread(this, s,
-				        curClientID++);
+						curClientID++);
 				threads.add(serverThread);
 				serverThread.start();
 				// start the timer
@@ -74,37 +84,26 @@ public class Server {
 					startTime = System.currentTimeMillis();
 				}
 			}
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.err.println("Error while listening: " + e);
 			System.exit(1);
 		}
 	}
+	
 	public synchronized void setHighest(Board b) {
 		if (highest == null || b.getScore() > highest.getScore()) {
 			highest = b;
 			System.err.println(highest);
-			if (highest.getScore() >= 3500) {
-				reset();
-				return;
-			}
-		}
-		
-		if (System.currentTimeMillis() - startTime >= 1000000) {
-			reset();
-		}
-		
-		if (trials >= 100) {
-			System.exit(0);
 		}
 	}
+	
 	public synchronized void removeThread(ServerThread t) {
 		threads.remove(t);
 	}
+	
 	private void reset() {
-		trials++;
 		System.out.println(highest + " "
-		        + Long.toString(System.currentTimeMillis() - startTime));
+				+ Long.toString(System.currentTimeMillis() - startTime));
 		// reset state
 		highest = null;
 		for (ServerThread t : threads) {
